afe6fb5c5c479f379f28a1b07793ab9e631d105d
kingdapster <sal.carrazco89@gmail.com>
2023-10-31 11:54:14 -0700

    index html and index js has been created and prompt log has been codded

---
+++ index.js
@@ -0,0 +1,9 @@
+//create a prompot 
+// log prompt 
+const logOfFlavors = prompt(
+    `Enter the flavors you would like to order. `
+)
+
+//put numbers in to string array 
+const stringArray = logOfFlavors.split (`,`);
+console.log(logOfFlavors)
