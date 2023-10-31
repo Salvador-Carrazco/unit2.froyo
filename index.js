b88aff0e84d7b2404c7d54e309a336253e690daf
kingdapster <sal.carrazco89@gmail.com>
2023-10-31 14:05:53 -0700

    added loop and data collection to object

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 //create a prompot 
 // log prompt 
+//split logg into array
 const logOfFlavors = prompt(
-    `Enter the flavors you would like to order. `
-)
+    `Enter the flavors you would like to order. Please seperate by commas. `,
+    `vanilla,vanilla,vanilla,strawberry,coffee,coffee`
+).split(`,`)
+//create object to collect array data 
+let flavors = {}
+logOfFlavors.data = flavors 
+//create loop for object 
+for (let i =0; i < logOfFlavors.length; i++){
+if (!flavors[logOfFlavors[i]]){ 
+    flavors[logOfFlavors[i]] = 1; 
+}else{
+    flavors[logOfFlavors[i]] += 1
+}
+
+}
+console.log(flavors)
+
+
+//  const flavors = {}
+// for(i=0; i < logOfFlavrs.length; i++)
+// if (!stringArray[i]){
+//     flavors[i] += 1
+// }
+
+
+
+
+// if(!stringArray[i]){
+//     logOfFlavors[i]=1
+// }
+
 
 //put numbers in to string array 
-const stringArray = logOfFlavors.split (`,`);
-console.log(logOfFlavors)
+
+// for (let i=0; i<stringArray.length;i++){
+//     const str = stringArray[i]
+    
+// }
+
+
+// console.log(logOfFlavors)
+
+
